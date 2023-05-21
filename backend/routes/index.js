dc29ac36308ce0a977937c34b5d47ad709654a30
Ilya Biryulev <birulevila@yandex.ru>
2023-05-21 18:11:05 +0300

    Редактировать README

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,6 +10,12 @@ const LINK = require('../utils/constants');
 const auth = require('../middlewares/auth');
 const { login, createUser } = require('../controllers/users');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
